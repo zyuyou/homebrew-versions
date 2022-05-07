07bf15459322bdfe48d30de574da824be23d4af5
zyuyou <yuyouchow@gmail.com>
2022-05-08 02:01:32 +0800

    firtst commit

---
+++ Formula/oh-my-posh@7.79.2.rb
@@ -0,0 +1,36 @@
+class OhMyPoshAT7792 < Formula
+  desc "Prompt theme engine for any shell"
+  homepage "https://ohmyposh.dev"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v7.79.2.tar.gz"
+  head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
+  sha256 "a8bcd136675755e85ac2eb5d0d23ad76263689148b92241cd6cff5e085b0820a"
+  license "MIT"
+  version "7.79.2"
+
+  depends_on "go@1.18" => :build
+
+  def install
+    Dir.chdir("src") do
+      ENV["GOPROXY"] = ENV.has_key?("HOMEBREW_GOPROXY") ? ENV["HOMEBREW_GOPROXY"] : ""
+      system("go build -o=oh-my-posh -ldflags=\"-s -w -X \'main.Version=7.79.2\'\"")
+      bin.install "oh-my-posh"
+    end
+    mv "themes", prefix
+  end
+
+  def caveats
+    <<~EOS
+      Thanks for installing Oh my Posh.
+      Have a look at https://ohmyposh.dev/docs/ for detailed instructions for your shell.
+      Sample themes can be found at $(brew --prefix oh-my-posh)/themes.
+
+      If you're enjoying Oh my Posh, feel free to donate or become a sponsor.
+      https://github.com/sponsors/JanDeDobbeleer
+    EOS
+  end
+
+  test do
+    system "#{bin}/oh-my-posh", "--help"
+  end
+end
+
